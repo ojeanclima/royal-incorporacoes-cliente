9bc79edd13200dc99f18e149a07cadbe530afdba
Jean Nunes <jean.carlos.cmo@gmail.com>
2025-07-10 00:00:27 -0300

    d

--- wp-content/plugins/jet-engine/assets/js/admin/blocks-views/src/common/functions.js
+++
@@ -1,9 +0,0 @@
-function isIdUnique( props ) {
-	const blockEditor = wp.data.select('core/block-editor');
-	const clientIds = blockEditor.getBlocksByName( props.name );
-	const blocks = blockEditor.getBlocksByClientId( clientIds );
-
-	return !blocks.some( ( block ) => block.clientId !== props.clientId && block.attributes._block_id === props.attributes._block_id );
-}
-
-export { isIdUnique };
